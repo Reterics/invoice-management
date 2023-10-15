ab3291adc2bb383314042f496ff9c33871cb35bc
Attila Reterics <reterics.attila@gmail.com>
2023-10-15 17:32:12 +0200

    Added LeJS for XML Templates

--- app/api/invoices/[id]/xml/route.ts
+++ app/api/invoices/[id]/xml/route.ts
@@ -1,6 +1,7 @@
 import {getById} from "@/src/firebase/config";
 import { promises as fs } from 'fs';
 import {Invoice} from "@/src/types/general";
+import {render} from '@redaty/lejs';
 
 export async function GET(
     request: Request,
@@ -8,20 +9,13 @@ export async function GET(
 ) {
     const invoice = await getById(params.id,
         process.env.NEXT_PUBLIC_FIREBASE_DB_INVOICES || 'invoices') as Invoice;
-    const xmlTemplate = await fs.readFile(process.cwd() + '/src/templates/simplified.xml', 'utf8');
+    let body = invoice.invoiceCategory ?
+            (await fs.readFile(process.cwd() + '/src/templates/' + invoice.invoiceCategory.toLowerCase() + '.xml',
+                'utf8')) : '<?xml version="1.0" encoding="UTF-8"?>';
 
-    let body = '<?xml version="1.0" encoding="UTF-8"?>';
     if (invoice) {
-        body = xmlTemplate;
-        Object.keys(invoice).forEach((key: string) => {
-            // @ts-ignore
-            if (typeof invoice[key] === 'string') {
-                // @ts-ignore
-                body = body.replaceAll('{{'+key+'}}', invoice[key]);
-            }
-        })
-        return new Response(body, { headers: { "Content-Type": "text/xml" } });
-
+        body = render(body, invoice, undefined);
     }
+
     return new Response(body, { headers: { "Content-Type": "text/xml" } });
 }
